62e1ec6351922d2aa22738300ef89e78adee1c9c
Asia Carter <asiamcarter@gmail.com>
2019-01-04 16:01:24 -0600

    song list loads with button click using jquery

---
+++ songs.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+    // Use jQuery to get a reference to `load-songs`
+  $("#load-songs")[0]
+
+
+    // Use jQuery to get a reference to `song-list`
+    $("#song-list")[0]
+
+    /*
+        Attach a click handler to the button with jQuery. When
+        the button is clicked, use $.ajax() to load `songs.json`
+        from the file system
+    */
+    $("#load-songs").click (function() {
+        $.ajax({
+            url: "http://localhost:8088/songs",
+            type: "GET",
+            success: function (song) {
+                console.log(song)
+            }
+        })
+
+
+    })
+
+    /*
+        Chain a `.then()` method to the ajax call, and when
+        it is complete build a DOM component for each song with
+        the following structure. Use the jQuery append() method
+        to put an HTML representation of each song the DOM as a
+        child component of the .
+
+            <section class="song">
+                <h1 class="song__title">{Title of song}</h1>
+                <section class="song__description">
+                    Performed by {artist} on the album {album}
+                </section>
+            </section>
+    */
+})
